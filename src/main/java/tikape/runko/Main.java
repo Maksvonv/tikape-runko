55fac0167da87a548ca1fda661e709c742c798d7
Maksvonv <max.morsky@gmail.com>
2016-06-17 16:24:55 +0300

    Jotain

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import spark.ModelAndView;
 import static spark.Spark.*;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
+import tikape.runko.database.AlueDao;
 import tikape.runko.database.AvausDao;
 import tikape.runko.database.Database;
 import tikape.runko.database.OpiskelijaDao;
@@ -19,13 +20,15 @@ public class Main {
         String a = "66KES88";
 
         OpiskelijaDao opiskelijaDao = new OpiskelijaDao(database);
+        
         AvausDao avausDao = new AvausDao(databaseU);
+        AlueDao alueDao = new AlueDao(databaseU);
         
         
 
         get("/", (req, res) -> {
             HashMap map = new HashMap<>();
-            map.put("viesti", "tervehdys");
+            map.put("alueet", alueDao.findAll());
 
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
